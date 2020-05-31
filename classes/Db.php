a3431b653c2fdd662bb823f70f4299cf0a678d43
Yury Sergeyev <60914627+Yorksergeyev@users.noreply.github.com>
2020-05-31 17:16:30 +0300

    Add files via upload

--- classes/Db.php
+++ classes/Db.php
@@ -11,4 +11,15 @@ class Db
         }
         return $connect;
     }
+    public static function get_single_element($sql_statement){
+        $result = self::getdbconnect()-> query($sql_statement);
+        $data = $result->fetch_row();
+        return $data[0];
+}
+
+public static function send_query($sql_statement){
+    $result = self::getdbconnect()-> query($sql_statement);
+    return $result;
+}
+
 }
